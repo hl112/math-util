a062b7f2f997cc9ae559e34c2b8c8a5e4baacf37
hl112 <forest.tl112@gmail.com>
2020-11-12 08:36:37 +0700

    1st upload

---
+++ src/lamhdt/util/MathUtility.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lamhdt.util;
+
+/**
+ *
+ * @author HL
+ */
+public class MathUtility {
+
+    //class này chứa các hàm tiện ích dùng chung cho mọi nơi
+    //do đó nên là static
+    public static final double PI = 3.1415;
+
+    //hàm tính n giai thừa. n >= 0 && n <= 20
+    // nếu quá 20 tràn kiểu long
+    /// 0! = 1; 1! = 1; n! = 1.2.3...n
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("n must be >= 0 & <= 20");
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+import lamhdt.util.MathUtility;
+
+/**
+ *
+ * @author HL
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //2. test hàm  = mắt   
+        System.out.println("5!:" + MathUtility.getFactorial(5));
+       //  expected: 120,     actual: 120
+       // hi vong : 120, 
+        System.out.println("0!:" + MathUtility.getFactorial(0));
+        //  expected: 1,     actual: 1
+       // hi vong : 1, 
+       
+       //e: ném ngoại lệ IllegalArgumentException
+       System.out.println("-5!:" + MathUtility.getFactorial(-5));
+       
+    }
+    
+     // code viết ra thì DEV phải test
+        // có vài hình thức test 
+        // 1. TDD Test Driven Development
+        // Viết Dàn Khung Của Hàm getFactorial(int n)
+        // sau đó dựng hàm test()
+        // chạy thử coi xanh đỏ như thế trong quá trình viết code và sửa code
+        // lập trình hướng theo thỏa mãn việc test, TDD
+    
+        // 2. sout  kèm data để coi kqua, có đúng như mình kì vọng không
+    
+        // xem kqua test = mắt
+        // JoptionPane = mắt
+        // web gọi hàm xly mlem các thứ 
+    
+    
+}
