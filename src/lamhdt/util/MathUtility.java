cb900917e068eb8147a6fc6b6fc18cd7b1bfbfa9
hl112 <forest.tl112@gmail.com>
2020-11-19 08:34:00 +0700

    DeQuiiii

--- src/lamhdt/util/MathUtility.java
+++ src/lamhdt/util/MathUtility.java
@@ -18,6 +18,19 @@ public class MathUtility {
     //hàm tính n giai thừa. n >= 0 && n <= 20
     // nếu quá 20 tràn kiểu long
     /// 0! = 1; 1! = 1; n! = 1.2.3...n
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("n must be >= 0 & <= 20");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
     public static long getFactorial(int n) {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("n must be >= 0 & <= 20");
@@ -25,10 +38,7 @@ public class MathUtility {
         if (n == 0 || n == 1) {
             return 1;
         }
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        
+        return n * getFactorial(n - 1); // dq 
     }
 }
